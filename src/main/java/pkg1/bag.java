1f10a6b42c9ee499a214561a8aadc3e7990f976d
SureshKumarDachepalli <sureshkumar.d467@gmail.com>
2024-02-19 16:03:28 +0530

    jenkinpp

---
+++ src/main/java/pkg1/bag.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class bag {
+
+	public static void main(String[] args) {
+		System.out.println("practice for jenkin bag");
+
+	}
+
+}
---
+++ src/test/java/pkg2/collegeBag.java
@@ -0,0 +1,10 @@
+package pkg2;
+
+public class collegeBag {
+
+	public static void main(String[] args) {
+		System.out.println("practice for jenkin collagebag");
+
+	}
+
+}
